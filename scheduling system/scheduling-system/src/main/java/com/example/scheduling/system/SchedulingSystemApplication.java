b6c568e86845b5ce6e66b61a4b5843bbe57b45bc
Dulanji_ADL <dulanji.cooray@axiatadigitallabs.com>
2024-09-18 20:39:33 +0530

    Initial Project Setup and Data Base

---
+++ scheduling system/scheduling-system/src/main/java/com/example/scheduling/system/SchedulingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.scheduling.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example.scheduling.system")
+public class SchedulingSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingSystemApplication.class, args);
+	}
+}
+
+
+
---
+++ scheduling system/scheduling-system/src/main/java/com/example/scheduling/system/entity/PasswordRequest.java
@@ -0,0 +1,14 @@
+package com.example.scheduling.system.entity;
+
+public class PasswordRequest {
+    private String password;
+
+    // Getter and Setter
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
